2960703ecb42be33ae68564ad65243cbe822b395
junseongday <junseongday@gmail.com>
2020-07-02 23:25:19 +0900

    delegate practice done

--- SignUpForm/ViewController.swift
+++ SignUpForm/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var telTextField: UITextField!
+    @IBOutlet weak var blogTextField: UITextField!
+    @IBOutlet weak var outputTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        print("textFieldShouldReturn")
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        outputTextView.text = "\(nameTextField.text!)님 가입을 축하합니다."
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        print("touchesBegan")
+        self.view.endEditing(true)
+    }
+    
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        print("touchesMoved")
+//    }
+//
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        print("touchesEnded")
+//    }
 }
 
